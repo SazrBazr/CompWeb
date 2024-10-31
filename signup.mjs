ef5598a44e3ae0384702452de140ed2932e16a2f
sari.zrieq <102485274+SazrBazr@users.noreply.github.com>
2024-10-31 21:56:25 +0200

    Add files via upload

--- signup.mjs
+++ signup.mjs
@@ -4,12 +4,12 @@ import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDzVUFQM-GT_Pxk1m8KHGyE2lfX4Fkihek",
-  authDomain: "notiapp-ff8b0.firebaseapp.com",
-  projectId: "notiapp-ff8b0",
-  storageBucket: "notiapp-ff8b0.firebasestorage.app",
-  messagingSenderId: "259790595328",
-  appId: "1:259790595328:web:d3d455adeb81a413217fe5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
